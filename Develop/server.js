ef6ffa3a4363b446c67c9be0dc4744ffe407e97f
jmacker101 <jm470t@gmail.com>
2024-05-10 01:04:41 -0400

    finished the first part of server.js now on to the second half of server.js

---
+++ Develop/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const routes = require('./routes');
+// import sequelize connection
+// taking line 3 - 13 in connections.js and putting it in server.js to import the seqelize
+
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'postgres',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
